b627b704ea92dd8dca60f7efd9e6cb2b35056621
danil <danilucheba102@gmail.com>
2023-05-25 22:38:35 +0500

    added removeProduct admin func and changed banner at main page

--- src/http/productAPI.ts
+++ src/http/productAPI.ts
@@ -1,6 +1,10 @@
-import {$host} from "./index";
+import {$authHost, $host} from "./index";
 
 export const getAllProduct = async () => {
     const {data} = await $host('api/product')
     return data.rows
+}
+export const removeProduct = async (productId: any) => {
+    const {data} = await $authHost.delete('api/product/delete', {data: {productId}})
+    return data
 }
--- src/pages/admin-panel/remove-product/index.tsx
+++ src/pages/admin-panel/remove-product/index.tsx
@@ -1,17 +1,32 @@
 import {Select, Stack} from "@mantine/core";
+import {useEffect, useState} from "react";
+import {getAllProduct, removeProduct} from "../../../http/productAPI.ts";
 
 const RemoveProduct = () => {
+    const [check, setCheck] = useState<any>('')
+    const [productData, setProductData] = useState<any>([]);
+    useEffect(() => {
+        const data = getAllProduct().then((data) => setProductData(data))
+    }, [])
+    const deleteProduct = async () => {
+        if (check === '') alert('Выберите продукт')
+        else try{
+            const response = removeProduct(check)
+        }catch(e){
+            alert('Ошибка удаления продукта')
+        }
+    }
     return (
         <div>
             <Stack align={'center'}>
-                <Select w={300} placeholder="Выберите продукт"
+                <Select w={300} placeholder="Выберите продукт" value={check} onChange={(value) => setCheck(value)}
                         data={[
                             { value: '1', label: 'Холодные закуски' },
                             { value: '2', label: 'Паста' },
                             { value: '3', label: 'Салаты' },
                             { value: '4', label: 'Супы' },
                         ]}/>
-                <button className={'CartBtn'} style={{backgroundColor: '#c72626', width: 300}}>Удалить продукт</button>
+                <button onClick={() => deleteProduct()} className={'CartBtn'} style={{backgroundColor: '#c72626', width: 300}}>Удалить продукт</button>
             </Stack>
         </div>
     );
