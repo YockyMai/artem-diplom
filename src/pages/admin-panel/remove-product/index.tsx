8abb8b156503a0c55624257973969dd48c2d6547
DanilFrontend <danilstorage@gmail.com>
2023-05-26 13:33:19 +0500

    fixed remove product

--- src/pages/admin-panel/remove-product/index.tsx
+++ src/pages/admin-panel/remove-product/index.tsx
@@ -6,13 +6,16 @@ const RemoveProduct = () => {
     const [check, setCheck] = useState<any>('')
     const [productData, setProductData] = useState<any>([]);
     useEffect(() => {
-        const data = getAllProduct().then((data) => setProductData(data))
+        getAllProduct().then((data) => setProductData(data))
     }, [])
     const deleteProduct = async () => {
         if (check === '') alert('Выберите продукт')
-        else try{
-            const response = removeProduct(check)
-        }catch(e){
+        else try {
+            await removeProduct(check)
+            setCheck('')
+            getAllProduct().then((data) => setProductData(data))
+            alert('Удаление продукта прошло успешно!')
+        } catch (e) {
             alert('Ошибка удаления продукта')
         }
     }
@@ -20,13 +23,10 @@ const RemoveProduct = () => {
         <div>
             <Stack align={'center'}>
                 <Select w={300} placeholder="Выберите продукт" value={check} onChange={(value) => setCheck(value)}
-                        data={[
-                            { value: '1', label: 'Холодные закуски' },
-                            { value: '2', label: 'Паста' },
-                            { value: '3', label: 'Салаты' },
-                            { value: '4', label: 'Супы' },
-                        ]}/>
-                <button onClick={() => deleteProduct()} className={'CartBtn'} style={{backgroundColor: '#c72626', width: 300}}>Удалить продукт</button>
+                        data={productData.map((obj: any) => ({value: `${obj.id}`, label: `${obj.name}`}))}/>
+                <button onClick={() => deleteProduct()} className={'CartBtn'}
+                        style={{backgroundColor: '#c72626', width: 300}}>Удалить продукт
+                </button>
             </Stack>
         </div>
     );
